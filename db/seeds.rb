d01ce06e171599a4e3d4b56d20c0581acfa77098
zuberzuzanna@gmail.com <zzuziak>
2019-02-20 21:25:12 +0100

    update seeds and css

--- db/seeds.rb
+++ db/seeds.rb
@@ -64,11 +64,10 @@ PR = Experience.create!({
   name: "one",
   title: "Public Relations",
   description: "My career started in the public relations industry. I worked as a Junior Account Executive responsbile for:
-  - writing PR publications and maintaining media relations ,
+  - writing PR publications and maintaining media relations,
   - contact with the customers,
-  - acreditation for various events (PS4 Premiere, Nitro Circus).
-
-  In general I'm really good with words and peoples relations",
+  - acreditation for various events (PS4 Premiere, Nitro Circus).<br>
+  In general I'm really good with <strong>words and peoples relations.</strong>",
   date: "2013"
   })
 
@@ -93,7 +92,7 @@ Fullstack = Experience.create!({
   title: "Fullstack developer",
   description: "From marketing/product manager to coding? <strong>Why not?</strong>
   <br>
-  I did a coding bootcamp with Le Wagon that introduced me to the coding world, Ruby on Rails to be precise. And I fell in love. Coding turned out to be something that brings me fulfilment and crazy satisfaction.<br>
+  I did a coding bootcamp with Le Wagon that introduced me to the coding world, <strong>Ruby on Rails</strong> to be precise. And I fell in love. Coding turned out to be something that brings me fulfilment and crazy satisfaction.<br>
   I'm currently working as a Junior Fullstack Developer. My daily stack includes <strong> Python, JS, PSQL.</strong> APIs are my best friends, including 3rd party integrations of course.<br>
   In my spare time I work on side projects such as this site.",
   date: "as of Nov 2018"
