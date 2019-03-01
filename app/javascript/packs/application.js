adaacd4580dc9340f46e3e4b693d9663ea2e6a6c
zuberzuzanna@gmail.com <zzuziak>
2019-03-01 08:09:22 +0100

    remove ping

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@ import "bootstrap";
 import { loadDynamicBannerText } from '../components/role_typed';
 loadDynamicBannerText();
 
-import { ping } from '../components/ping';
-ping();
+// import { ping } from '../components/ping';
+// ping();
 
 import { timeline } from '../components/timeline';
